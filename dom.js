6a5c99db2769b2e252a81f54b7bfc8c6f8db7f48
tharun1220 <tharun1220@gmail.com>
2023-01-20 21:16:59 +0530

    code 1

---
+++ dom.js
@@ -0,0 +1,22 @@
+// EXAMINE THE DOCUMENT OBJECT
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title=123;
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+//GET ELEMENT BY ID
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-tile');
+var header = document.getElementById('main-header');
+console.log(headerTitle);
+header.style.borderBottom = 'solid 3px #000';
