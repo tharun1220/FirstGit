fdee613c6ce8298df418714aa4e12af52d6709b9
tharun1220 <tharun1220@gmail.com>
2023-01-25 23:24:32 +0530

    code5

--- GitFile/index.js
+++ GitFile/index.js
@@ -1,8 +1,16 @@
-function myfunc(event)
-{
-    event.preventDefault();
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
-}
+ function myfunc(event)
+ {
+     event.preventDefault();
+     var name = document.getElementById("name").value;
+     var email = document.getElementById("email").value;
+     localStorage.setItem('name',name);
+     localStorage.setItem('email',email);
+ }
+let myObj = {
+    name : "abc",
+    age : 25
+};
+
+let myObj_serialized = JSON.stringify(myObj);
+localStorage.setItem('myObj',myObj_serialized);
+console.log(localStorage);
