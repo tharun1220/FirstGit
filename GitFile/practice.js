057a073069ca841c0f53bc8a283225f4d05829fb
tharun1220 <tharun1220@gmail.com>
2023-02-06 16:40:14 +0530

    code 6

---
+++ GitFile/practice.js
@@ -0,0 +1,24 @@
+var number1 = 2;
+
+var number2 = 5
+
+
+
+function add(a, b){
+
+var answer = a + b; //line 1
+
+return answer;
+
+}
+
+var sum = add(number1, number2); // line 2
+
+
+
+console.log(sum) //line 3
+
+
+
+console.log(answer) //line 4
+
