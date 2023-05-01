8b75cb5ddfb3dc512b6e4a1c11dfbc7f208a0f5f
tharun1220 <tharun1220@gmail.com>
2023-05-01 22:03:56 +0530

    creating first server and sending response

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,11 @@
 const http = require('http')
 const server = http.createServer((req,res) =>{
-    console.log('Hi Tharun')
+    console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title> My First page </title></head>')
+    res.write('<body><h1> Hello from my Node.js server! </h1></body>')
+    res.write('</html>')
+    res.end();
 });
 server.listen(4000)
