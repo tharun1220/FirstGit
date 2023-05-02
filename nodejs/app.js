cf7aa3ee58e23ff3b3c4b6137457474af19bb83c
tharun1220 <tharun1220@gmail.com>
2023-05-02 15:44:33 +0530

    writing and reading a file

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,6 +1,33 @@
 const http = require('http')
+const fs = require('fs');
 const server = http.createServer((req,res) =>{
-    console.log(req.url, req.method, req.headers)
+    const url = req.url;
+    const method = req.method;
+    if(url === '/')
+    {
+        res.write('<html>')
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url === '/message' && method === 'POST')
+    {
+        const body =[]
+        req.on('data',chunk =>{
+            console.log(chunk)
+            body.push(chunk)
+        });
+        return req.on('end',() =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt',message, err =>{
+                res.statusCode = 302;
+                res.setHeader('Location','/')
+                return res.end()
+            })
+        })
+    }
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title> My First page </title></head>')
@@ -8,4 +35,4 @@ const server = http.createServer((req,res) =>{
     res.write('</html>')
     res.end();
 });
-server.listen(4000)
+server.listen(3000)
