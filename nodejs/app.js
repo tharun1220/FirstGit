1d5e12ec2757d7a65b99b75b24adc8f3ffd6d531
tharun1220 <tharun1220@gmail.com>
2023-05-03 09:23:22 +0530

    writing and reading from a file

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,32 +1,41 @@
-const http = require('http')
+const http = require('http');
 const fs = require('fs');
-const server = http.createServer((req,res) =>{
+
+const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if(url === '/')
     {
-        res.write('<html>')
-        res.write('<head><title>Enter message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>')
-        res.write('</html>')
-        return res.end()
+        return fs.readFile('message.txt', { encoding: 'utf-8' }, (err, data) =>{
+            if(err)
+            {
+                console.log(err);
+            }
+            console.log(data)
+            res.write('<html>');
+            res.write('<head><title>Enter message</title></head>');
+            res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`);
+            res.write(`<body>${data}</body>`);
+            res.write('</html>');
+            return res.end();
+        });
     }
-    if(url === '/message' && method === 'POST')
+    else if(url === '/message' && method === 'POST')
     {
-        const body =[]
-        req.on('data',chunk =>{
-            console.log(chunk)
-            body.push(chunk)
+        const body = [];
+        req.on('data',(chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
         });
         return req.on('end',() =>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message, err =>{
+            fs.writeFile('message.txt', message, (err) =>{
                 res.statusCode = 302;
-                res.setHeader('Location','/')
-                return res.end()
-            })
-        })
+                res.setHeader('Location','/');
+                return res.end();
+            });
+        });
     }
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
@@ -35,4 +44,5 @@ const server = http.createServer((req,res) =>{
     res.write('</html>')
     res.end();
 });
-server.listen(3000)
+
+server.listen(4000)
