6228a7cd850326cf413d82e66cab15ee4d38b239
tharun1220 <tharun1220@gmail.com>
2023-05-26 13:47:28 +0530

    Express code 1

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,18 +1,21 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log("In the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use( '/add-product', (req, res, next) =>{
+    res.send('<form action ="/product" method ="POST"><input type ="text" name ="title"><button type ="submit">Add product</button></form>');
 })
 
-app.use((req, res, next) =>{
-    console.log("In the another middleware");
-    res.send('<h1>Hello from express!</h1>')
-    // ...
+app.post('/product', (req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) =>{
+    res.send('<h1>Hello from express!</h1>');
 })
 
 app.listen(4000)
